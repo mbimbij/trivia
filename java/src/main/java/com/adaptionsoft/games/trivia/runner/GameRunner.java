96e52a46eaa69da0f5fba28c0e2793e016210280
mbimbij <joseph.mbimbi@gmail.com>
2024-02-08 23:54:06 +0100

    Setup a test harness

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -8,29 +8,43 @@ import com.adaptionsoft.games.uglytrivia.Game;
 public class GameRunner {
 
 	private static boolean notAWinner;
+	private final Random rand;
+
+	public GameRunner() {
+		rand = new Random();
+	}
+
+	public GameRunner(int seed) {
+		rand = new Random(seed);
+	}
 
 	public static void main(String[] args) {
+		new GameRunner().doRun();
+	}
+
+	public void doRun() {
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
+
+			aGame.roll(getDiceRoll(rand));
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
+	}
+
+	private static int getDiceRoll(Random rand) {
+		return rand.nextInt(5) + 1;
 	}
 }
--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,14 +1,34 @@
 package com.adaptionsoft.games.trivia;
 
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeTest {
 
     @Test
-    public void true_is_true() throws Exception {
-        assertTrue(false);
+    public void should_not_differ_from_golden() throws IOException {
+        // GIVEN
+        int seed = 777;
+        System.setOut(new PrintStream("src/test/resources/lead.txt"));
+        String gold = Files.readString(Paths.get("src/test/resources/gold.txt"));
+
+        // WHEN
+        new GameRunner(seed).doRun();
+
+        // THEN
+        String lead = Files.readString(Paths.get("src/test/resources/lead.txt"));
+        assertEquals(gold, lead);
     }
 }
