96d363ae653affdd1a9fef54dd390bf982f9e1bc
mbimbij <joseph.mbimbi@gmail.com>
2024-06-07 18:04:29 +0200

    Restore firebase API key as it is not a secret

--- java/frontend-angular/src/environments/environment.template.ts
+++
@@ -1,8 +0,0 @@
-export const environment =
-  {
-    production: false,
-    apiUrl: 'http://localhost:8080',
-    firebaseConfig: {
-      apiKey: '$apiKey',
-    }
-  };
---
+++ java/frontend-angular/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  backendUrl: 'http://localhost:8081',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBgVgToHnITb86RRrQYX7-s2vWJoa3IDEw',
+  }
+};
