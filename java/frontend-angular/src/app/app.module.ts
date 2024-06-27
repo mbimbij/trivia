86ef7c7cb9b1dc5296b7c9c37bcb81ded8604961
mbimbij <joseph.mbimbi@gmail.com>
2024-06-27 19:33:12 +0200

    Fix backend url in frontend for prod

--- java/frontend-angular/src/app/app.module.ts
+++ java/frontend-angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {GameServiceAbstract} from "./services/game-service-abstract";
 import {GameService} from "./game/game.service";
-import {ApiModule} from "./openapi-generated";
+import {ApiModule, BASE_PATH, Configuration} from "./openapi-generated";
 import {CreateGameComponent} from "./game/create-game/create-game.component";
 import {FormsModule} from "@angular/forms";
 import {AppRoutingModule} from './app-routing.module';
@@ -18,7 +18,7 @@ import {environment} from "../environments/environment";
 import {AngularFireAuthModule, USE_EMULATOR as USE_AUTH_EMULATOR} from "@angular/fire/compat/auth";
 import {FirebaseUIModule} from "firebaseui-angular";
 import {firebaseUiAuthConfig} from "./adapters/authentication/firebase-ui-auth-config";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {AuthenticationServiceAbstract} from "./services/authentication-service.abstract";
 import {FirebaseAuthenticationService} from "./adapters/authentication/firebase-authentication.service";
 import {FirebaseUserService} from "./adapters/user/firebase-user.service";
@@ -45,10 +45,11 @@ import {UserServiceAbstract} from "./services/user-service.abstract";
     {provide: AuthenticationServiceAbstract, useClass: FirebaseAuthenticationService},
     {provide: UserServiceAbstract, useClass: FirebaseUserService},
     {provide: RxStompService, useFactory: rxStompServiceFactory},
+    {provide: BASE_PATH, useValue: environment.backendUrl},
     provideAuth(() => getAuth()),
     {
       provide: USE_AUTH_EMULATOR,
-      useValue:  undefined
+      useValue: undefined
       // useValue: !environment.production ? ['http://localhost:9099'] : undefined
     },
     provideAnimationsAsync(),
--- java/frontend-angular/src/environments/environment.ts
+++ java/frontend-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  backendUrl: `http://localhost:8080/api`,
+  backendUrl: `http://localhost:8080`,
   backendWebSocketUrl: `ws://localhost:8080/ws/gs-guide-websocket`,
   firebaseConfig: {
     apiKey: 'AIzaSyBgVgToHnITb86RRrQYX7-s2vWJoa3IDEw',
