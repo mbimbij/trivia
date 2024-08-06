c01178b65a5849a27c7142e8afff5fffddbe083e
mbimbij <joseph.mbimbi@gmail.com>
2024-08-06 13:21:05 +0200

    Disable unused test

--- java/e2e-tests/src/test/java/com/adaptionsoft/games/tests/BackendTest.java
+++ java/e2e-tests/src/test/java/com/adaptionsoft/games/tests/BackendTest.java
@@ -5,11 +5,13 @@ import com.adaptionsoft.games.domain.pageObjects.Backend;
 import com.adaptionsoft.games.trivia.web.GameResponseDto;
 import com.adaptionsoft.games.trivia.web.UserDto;
 import com.adaptionsoft.games.trivia.web.WebConfig;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 class BackendTest {
     @Test
+    @Disabled
     void can_deserialize_response() {
         RestTemplate restTemplate = new E2eTestsSpringConfiguration().restTemplate(new WebConfig().stateDeserializer());
         Backend backend = new Backend(restTemplate, "http://localhost:8080/api");
