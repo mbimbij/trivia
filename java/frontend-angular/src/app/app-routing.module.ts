eb41dc09f375c209c518bb32c06b094025feadff
mbimbij <joseph.mbimbi@gmail.com>
2024-08-18 14:12:20 +0200

    Reorder routes declaration

--- java/frontend-angular/src/app/app-routing.module.ts
+++ java/frontend-angular/src/app/app-routing.module.ts
@@ -13,19 +13,19 @@ import { NotFoundComponent } from './common/not-found/not-found.component';
 import {HealthComponent} from "./health/health.component";
 
 const routes: Routes = [
-  {
-    path: "games", component: GameListComponent
-    , canActivate: [loginActivateGuard, emailVerifiedGuard]
-  },
-  {path: "", redirectTo: "/games", pathMatch: "full"},
-  {path: "games/:id", component: GameComponent, canActivate: [loginActivateGuard, emailVerifiedGuard]},
-  {path: "games/:id/details", component: GameDetailsComponent, canActivate: [loginActivateGuard, emailVerifiedGuard]},
   {path: "authentication", component: AuthenticationComponent},
   {
     path: "waiting-for-email-verification",
     component: WaitingForEmailVerificationComponent,
     canActivate: [loginActivateGuard]
   },
+  {
+    path: "games", component: GameListComponent
+    , canActivate: [loginActivateGuard, emailVerifiedGuard]
+  },
+  {path: "", redirectTo: "/games", pathMatch: "full"},
+  {path: "games/:id/details", component: GameDetailsComponent, canActivate: [loginActivateGuard, emailVerifiedGuard]},
+  {path: "games/:id", component: GameComponent, canActivate: [loginActivateGuard, emailVerifiedGuard]},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'health', component: HealthComponent},
   {path: '**', redirectTo: '/not-found'}
