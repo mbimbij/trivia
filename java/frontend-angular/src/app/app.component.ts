f349af2964b1e13423606c4f6895e46cd904b67c
mbimbij <joseph.mbimbi@gmail.com>
2024-09-18 12:16:47 +0200

    Define AppComponent change detection strategy to OnPush

--- java/frontend-angular/src/app/app.component.ts
+++ java/frontend-angular/src/app/app.component.ts
@@ -1,11 +1,16 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Identifiable} from "./common/identifiable";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent extends Identifiable {
   title: string = 'frontend-angular';
+
+  override checkRender(): string {
+    return super.checkRender();
+  }
 }
