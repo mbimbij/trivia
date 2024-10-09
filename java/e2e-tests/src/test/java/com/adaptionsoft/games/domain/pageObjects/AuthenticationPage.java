0f2906ac0e84f310a089a7c94e2e9bd40a91d9a4
mbimbij <joseph.mbimbi@gmail.com>
2024-10-09 17:28:41 +0200

    take screenshots

--- java/e2e-tests/src/test/java/com/adaptionsoft/games/domain/pageObjects/AuthenticationPage.java
+++ java/e2e-tests/src/test/java/com/adaptionsoft/games/domain/pageObjects/AuthenticationPage.java
@@ -1,7 +1,11 @@
 package com.adaptionsoft.games.domain.pageObjects;
 
 import com.microsoft.playwright.Page;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Slf4j
 public class AuthenticationPage extends PageWithStaticUrl {
@@ -17,12 +21,22 @@ public class AuthenticationPage extends PageWithStaticUrl {
         this.gamesListPage = gamesListPage;
     }
 
+    @SneakyThrows
     public void loginViaEmailAndPassword(String email, String password) {
         log.info("logging in");
         page.navigate(this.url);
         page.locator("css=" + EMAIL_PASSWORD_IDP_BUTTON).click();
         page.locator("css=" + EMAIL_INPUT).fill(email);
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(Path.of("screenshots/screenshot_fill_email"))
+                .setFullPage(true)
+        );
         page.locator("css=" + SUBMIT_BUTTON).click();
+        Thread.sleep(2000);
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(Path.of("screenshots/screenshot_submit_email"))
+                .setFullPage(true)
+        );
         page.locator("css=" + PASSWORD_INPUT).fill(password);
         gamesListPage.executeAndWaitForWebSocketMessages(() -> {
             page.locator("css=" + SUBMIT_BUTTON).click();
