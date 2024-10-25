4e39feba92a30e6ad8cd2d8e1210d4669cdbe831
mbimbij <joseph.mbimbi@gmail.com>
2024-10-25 13:04:32 +0200

    Improve visual of the user settings page

--- java/frontend-angular/src/app/user/user-settings/user-settings.component.ts
+++ java/frontend-angular/src/app/user/user-settings/user-settings.component.ts
@@ -2,21 +2,39 @@ import {Component, Input} from '@angular/core';
 import {UserServiceAbstract} from "../../services/user-service.abstract";
 import {Identifiable} from "../../shared/identifiable";
 import {AsyncPipe, NgIf} from "@angular/common";
+import {ids} from "../../ids";
+import {MatError, MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {NotBlankValidatorDirective} from "../../shared/validation/not-blank-validator.directive";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ValidationErrorCodes} from "../../shared/validation/validation-error-codes";
+import {MatDialogActions} from "@angular/material/dialog";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
   imports: [
     NgIf,
-    AsyncPipe
+    AsyncPipe,
+    MatInput,
+    NotBlankValidatorDirective,
+    ReactiveFormsModule,
+    FormsModule,
+    MatError,
+    MatFormField,
+    MatLabel,
+    MatDialogActions,
+    MatButton
   ],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.css'
 })
 export class UserSettingsComponent extends Identifiable{
-  @Input() userName!: string;
 
   constructor(protected userService: UserServiceAbstract) {
     super()
   }
+
+  protected readonly ids = ids;
+  protected readonly ValidationErrorCodes = ValidationErrorCodes;
 }
