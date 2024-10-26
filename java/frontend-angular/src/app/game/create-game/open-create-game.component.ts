7f820ecf573c6a3b28f6eb2438c8dd8a98741c6c
mbimbij <joseph.mbimbi@gmail.com>
2024-10-26 10:05:18 +0200

    Add a plus icon to the create button

--- java/frontend-angular/src/app/game/create-game/open-create-game.component.ts
+++ java/frontend-angular/src/app/game/create-game/open-create-game.component.ts
@@ -20,7 +20,9 @@ import {CreateGameDialogData} from "./create-game-dialog.data";
       [attr.data-testid]="ids.createGame.OPEN_DIALOG_BUTTON"
       class="rounded"
       mat-raised-button color="primary" (click)="openDialog(CreateGameDialogContentComponent)"
-    >create game
+    >
+      <span class="material-icons md-24">add</span>
+      <span style="margin-left: 0.3rem">create game</span>
     </button>
   `,
   styleUrl: './open-create-game.component.css',
