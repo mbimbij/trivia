386cf54b71b7210efc9db8e779574bdd3795833f
mbimbij <joseph.mbimbi@gmail.com>
2024-10-27 14:16:25 +0100

    Open user actions via a menu dropdown, much cleaner

--- java/frontend-angular/src/app/shared/navbar/navbar-user/navbar-user.component.ts
+++ java/frontend-angular/src/app/shared/navbar/navbar-user/navbar-user.component.ts
@@ -4,9 +4,8 @@ import {MatIcon} from "@angular/material/icon";
 import {Identifiable} from "../../identifiable";
 import {UserServiceAbstract} from "../../../services/user-service.abstract";
 import {AuthenticationServiceAbstract} from "../../../services/authentication-service-abstract";
-import {NavbarLogoutButtonComponent} from "./navbar-logout-button/navbar-logout-button.component";
 import {Router} from "@angular/router";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
@@ -14,13 +13,13 @@ import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
   standalone: true,
   imports: [
     AsyncPipe,
-    NavbarLogoutButtonComponent,
     MatIcon,
     NgIf,
     MatButton,
     MatMenu,
     MatMenuItem,
-    MatMenuTrigger
+    MatMenuTrigger,
+    MatIconButton
   ],
   templateUrl: './navbar-user.component.html',
   styleUrl: './navbar-user.component.css',
